5faf8f90452a1d866867b04bb72a347fac0f81c8
Mehdi Amiz <slaykeur2000@gmail.com>
2025-08-08 23:20:03 +0100

    Add admin user creation SQL

---
+++ create-admin.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (id, email, name, password_hash, credits, is_admin) 
+VALUES (
+  'admin-1733692800', 
+  'admin@nexodusai.com', 
+  'Admin User', 
+  '$2b$10$9yS6mfrwgRRrUUaTgFKEAeelx4ju0XiItjkEsP7emtZN4zNOv2Vvi', 
+  10000, 
+  true
+);
